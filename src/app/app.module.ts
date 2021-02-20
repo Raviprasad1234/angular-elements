7f167b88f125f161dd4b443fb15937d4bacb2a56
RaviPrasad Reddy <anuguravireddy@gmail.com>
2021-02-20 21:13:43 +0530

    code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule  , Injector, ApplicationRef} from '@angular/core';
 
+
+
 import { createCustomElement } from '@angular/elements';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrameworkPollComponent } from './framework-poll/framework-poll.component';
-
-
-
-
-import { ButtonComponent } from './button/button.component';
-
 
 
 
@@ -19,35 +17,26 @@ import { ButtonComponent } from './button/button.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FrameworkPollComponent,
-    ButtonComponent
+    FrameworkPollComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  entryComponents : [ButtonComponent]
+  entryComponents : [FrameworkPollComponent]
 })
 export class AppModule {
 
   constructor(private  injector : Injector){}
 
 
-  // ngDoBootstrap(appRef : ApplicationRef ){
-  //   const el = createCustomElement(FrameworkPollComponent, {injector : this.injector});
-  //   customElements.define('framework-poll' , el);
-  // }
-
-
-  ngDoBootstrap(){
-    const el = createCustomElement(ButtonComponent, {injector : this.injector});
-    customElements.define('app-button', el);
-
+  ngDoBootstrap(appRef : ApplicationRef ){
+    const el = createCustomElement(FrameworkPollComponent, {injector : this.injector});
+    customElements.define('framework-poll' , el);
   }
 
-
-}
+ }
 
 
 
